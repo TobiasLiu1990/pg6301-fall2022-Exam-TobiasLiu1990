f79f055d725ad4365a4d4164dfef504b4979721e
TobiasLiu1990 <toli025@student.kristiania.no>
2022-11-21 13:35:19 +0100

    Connecting to MongoDB to serve information

--- server/server.js
+++ server/server.js
@@ -1,8 +1,27 @@
 import express from "express";
 import * as path from "path";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import {MongoClient} from "mongodb";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//MongoDB connection
+const mongoDbClient = new MongoClient(process.env.MONGODB_URL);
+
+
+
+
+
+
+
+
+
 
 //Serve static files from dist
 app.use(express.static("../client/dist"));
